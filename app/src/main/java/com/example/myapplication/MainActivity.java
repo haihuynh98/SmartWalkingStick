72a0361c50c066c792d0c994c256e9bad00d72ab
Huỳnh Ngọc Hải <haihuynh.dev@gamil.com>
2021-01-30 17:35:11 +0700

    feat: Call contact
    - 30/01/2021

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,38 +1,59 @@
 package com.example.myapplication;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.ContentResolver;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.VibrationEffect;
 import android.os.Vibrator;
 import android.provider.ContactsContract;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.core.app.ActivityCompat;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 import java.util.ArrayList;
 
 public class MainActivity extends Activity {
     TextView textInfo;
+    TextView textContact;
     Button btnCheck;
     Button btnGetContact;
     HidBridge hidBridge;
     ListView list;
-    ArrayList mobileArray;
-    public static final int REQUEST_READ_CONTACTS = 79;
-    private static Context mContext;
+    ArrayList<ContactPhone> mobileArray;
+    int currentContactIndex = 0;
+    ContactPhone currentContact;
 
+    String[] PERMISSIONS = {
+            android.Manifest.permission.READ_CONTACTS,
+            Manifest.permission.CALL_PHONE,
+    };
+
+    private String currentPhone = "0389919130";
+
+    public static final int REQUEST_READ_CONTACTS = 79;
+    private static Activity mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (!hasPermissions(getContext(), PERMISSIONS)) {
+            requestPermission();
+        }
+
 
         setContext(this);
 
@@ -40,21 +61,27 @@ public class MainActivity extends Activity {
 
     }
 
-    private static void setContext(Context mContext) {
+    private static void setContext(Activity mContext) {
         MainActivity.mContext = mContext;
     }
 
-    public static Context getContext() {
+    public static Activity getContext() {
         return MainActivity.mContext;
     }
-
 
     //    Function setup interface composer on screen Activity Main
     private void setupComposer() {
         btnCheck = (Button) findViewById(R.id.btCheck);
-        btnGetContact = (Button) findViewById(R.id.btGetContact);
         textInfo = (TextView) findViewById(R.id.tvInfo);
         Vibrator vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+
+        // Hai
+        btnGetContact = (Button) findViewById(R.id.btGetContact);
+        textContact = (TextView) findViewById(R.id.tvContact);
+        Button btnUpScroll = (Button) findViewById(R.id.btScrollUp);
+        Button btnDownScroll = (Button) findViewById(R.id.btScrollDown);
+
+        FloatingActionButton btnFab = (FloatingActionButton) findViewById(R.id.fabCall);
 
         btnCheck.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -63,57 +90,73 @@ public class MainActivity extends Activity {
             }
         });
 
-        btnGetContact.setOnClickListener(new View.OnClickListener() {
+        if (hasPermissions(getContext(), Manifest.permission.READ_CONTACTS)) {
+            mobileArray = getAllContacts();
+
+            // Set current contact with contact at first in list
+            currentContact = new ContactPhone(mobileArray.get(0).getName(), mobileArray.get(0).getPhone());
+            setCurrentContact(0);
+
+        } else {
+            requestPermission();
+        }
+
+        btnFab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (ActivityCompat.checkSelfPermission(getContext(), android.Manifest.permission.READ_CONTACTS)
-                        == PackageManager.PERMISSION_GRANTED) {
-                    mobileArray = getAllContacts();
+                if (hasPermissions(getContext(), Manifest.permission.CALL_PHONE)) {
+                    Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + currentContact.getPhone()));
+                    startActivity(intent);
                 } else {
                     requestPermission();
                 }
-                list = findViewById(R.id.lvContact);
-                ArrayAdapter adapter = new ArrayAdapter<String>(getContext(),
-                        android.R.layout.simple_list_item_1, android.R.id.text1, mobileArray);
-                list.setAdapter(adapter);
+            }
+        });
+
+        btnUpScroll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (currentContactIndex > 0) {
+                    currentContactIndex--;
+                    setCurrentContact(currentContactIndex);
+                }
+            }
+        });
+
+        btnDownScroll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (currentContactIndex < mobileArray.size()) {
+                    currentContactIndex++;
+                    setCurrentContact(currentContactIndex);
+                }
             }
         });
     }
 
-
-    private void requestPermission() {
-        if (ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.READ_CONTACTS)) {
-            // show UI part if you want here to show some rationale !!!
-
-        } else {
-            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.READ_CONTACTS},
-                    REQUEST_READ_CONTACTS);
-        }
-        if (ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.READ_CONTACTS)) {
-        } else {
-            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.READ_CONTACTS},
-                    REQUEST_READ_CONTACTS);
-        }
+    private void setCurrentContact(int index) {
+        currentContact = mobileArray.get(index);
+        textContact.setText(currentContact.getName() + " (" + currentContact.getPhone() + ")");
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case REQUEST_READ_CONTACTS: {
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    mobileArray = getAllContacts();
-                } else {
-                    // permission denied,Disable the
-                    // functionality that depends on this permission.
+    public static boolean hasPermissions(Context context, String... permissions) {
+        if (context != null && permissions != null) {
+            for (String permission : permissions) {
+                if (ActivityCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
+                    return false;
                 }
-                return;
             }
         }
+        return true;
+    }
+
+    private void requestPermission() {
+        int PERMISSION_ALL = 1;
+        ActivityCompat.requestPermissions((Activity) getContext(), PERMISSIONS, PERMISSION_ALL);
     }
 
     private ArrayList getAllContacts() {
-        ArrayList<String> nameList = new ArrayList<>();
+        ArrayList<ContactPhone> ContactList = new ArrayList<>();
         ContentResolver cr = getContentResolver();
         Cursor cur = cr.query(ContactsContract.Contacts.CONTENT_URI,
                 null, null, null, null);
@@ -123,7 +166,7 @@ public class MainActivity extends Activity {
                         cur.getColumnIndex(ContactsContract.Contacts._ID));
                 String name = cur.getString(cur.getColumnIndex(
                         ContactsContract.Contacts.DISPLAY_NAME));
-                nameList.add(name);
+                String phoneNo = null;
                 if (cur.getInt(cur.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER)) > 0) {
                     Cursor pCur = cr.query(
                             ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
@@ -131,27 +174,30 @@ public class MainActivity extends Activity {
                             ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?",
                             new String[]{id}, null);
                     while (pCur.moveToNext()) {
-                        String phoneNo = pCur.getString(pCur.getColumnIndex(
+                        phoneNo = pCur.getString(pCur.getColumnIndex(
                                 ContactsContract.CommonDataKinds.Phone.NUMBER));
                     }
                     pCur.close();
+                }
+                if (phoneNo != null) {
+                    ContactList.add(new ContactPhone(name, phoneNo));
                 }
             }
         }
         if (cur != null) {
             cur.close();
         }
-        return nameList;
+        return ContactList;
     }
 
     private void checkInfo() {
-//        Vibrator v = (Vibrator) getSystemService(this.VIBRATOR_SERVICE);
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-//            v.vibrate(VibrationEffect.createOneShot(500, VibrationEffect.DEFAULT_AMPLITUDE));
-//        } else {
-//            //deprecated in API 26
-//            v.vibrate(500);
-//        }
+        Vibrator v = (Vibrator) getSystemService(this.VIBRATOR_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            v.vibrate(VibrationEffect.createOneShot(500, VibrationEffect.DEFAULT_AMPLITUDE));
+        } else {
+            //deprecated in API 26
+            v.vibrate(500);
+        }
 
         HidBridge hidBridge = new HidBridge(this, 390, 1112, textInfo);
         // hidBridge = new HidBridge(this,22352,1155,textInfo);
